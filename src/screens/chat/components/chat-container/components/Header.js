72037dc52f9fad9298d362d517075f80a3980db5
Anxhelo Cenollari <anxhelocenollari@Anxhelos-MacBook-Pro.local>
2024-12-17 17:14:08 +0100

    asdf

--- src/screens/chat/components/chat-container/components/Header.js
+++ src/screens/chat/components/chat-container/components/Header.js
@@ -5,53 +5,61 @@ import { HOST } from "../../../../../lib/utils";
 import { getColor } from "../../../../../utils/constants";
 
 const Header = () => {
-  const updateFuncChat = useAppStore(state => state.updateFuncChat)
-  const selectedChatData = useAppStore(state => state.selectedChatData)
-  const selectedChatType = useAppStore(state => state.selectedChatType)
+  const updateFuncChat = useAppStore((state) => state.updateFuncChat);
+  const selectedChatData = useAppStore((state) => state.selectedChatData);
+  const selectedChatType = useAppStore((state) => state.selectedChatType);
 
   return (
     <div className="h-[10vh] border-b-2 border-[#2f303d] flex items-center justify-between px-3">
       <div className="flex gap-5 items-center">
         <div className="flex gap-3 items-center justify-center">
           <div className="size-12 rounded-full overflow-hidden">
-            { selectedChatType === "contact" ? <>
-              {selectedChatData.image ? (
-                <img
-                  src={`${HOST}/${selectedChatData.image}`}
-                  alt="avatar"
-                  className="w-full h-full object-cover bg-black"
-                />
-              ) : (
-                <div
-                  className={` uppercase w-full h-full text-lg border-[1px] flex items-center justify-center rounded-full ${getColor(
-                    selectedChatData.color
-                  )}`}
-                >
-                  {selectedChatData.firstName
-                    ? selectedChatData?.firstName?.split("").shift()
-                    : selectedChatData?.email?.split("").shift()}
-                </div>
-              )}
-            </> : <div className="bg-[#ffffff22] w-full h-full text-lg border-[1px] rounded-full flex items-center justify-center text-white">#</div>}
+            {selectedChatType === "contact" ? (
+              <>
+                {selectedChatData.image ? (
+                  <img
+                    src={`${HOST}/${selectedChatData.image}`}
+                    alt="avatar"
+                    className="w-full h-full object-cover bg-black"
+                  />
+                ) : (
+                  <div
+                    className={` uppercase w-full h-full text-lg border-[1px] flex items-center justify-center rounded-full ${getColor(
+                      selectedChatData.color
+                    )}`}
+                  >
+                    {selectedChatData.firstName
+                      ? selectedChatData?.firstName?.split("").shift()
+                      : selectedChatData?.email?.split("").shift()}
+                  </div>
+                )}
+              </>
+            ) : (
+              <div className="bg-[#ffffff22] w-full h-full text-lg border-[1px] rounded-full flex items-center justify-center text-white">
+                #
+              </div>
+            )}
           </div>
-          {selectedChatType === "contact" && selectedChatData.firstName ? `${selectedChatData.firstName} ${selectedChatData.lastName}` : selectedChatData.email}
+          {selectedChatType === "contact" && selectedChatData.firstName
+            ? `${selectedChatData.firstName} ${selectedChatData.lastName}`
+            : selectedChatData.email}
           {selectedChatType === "channel" && selectedChatData.name}
         </div>
       </div>
-        <div className="flex items-center gap-5 justify-center">
-          <button
-            className="text-neutral-500 focus:border-none focus:outline-none focus:text-white duration-300 transition-all cursor-pointer"
-            onClick={() =>
-              updateFuncChat({
-                selectedChatType: null,
-                selectedChatData: null,
-                selectedChatMessages: [],
-              })
-            }
-          >
-            <IoIosClose className="text-3xl pointer-events-none" />
-          </button>
-        </div>
+      <div className="flex items-center gap-5 justify-center">
+        <button
+          className="text-neutral-500 focus:border-none focus:outline-none focus:text-white duration-300 transition-all cursor-pointer"
+          onClick={() =>
+            updateFuncChat({
+              selectedChatType: null,
+              selectedChatData: null,
+              selectedChatMessages: [],
+            })
+          }
+        >
+          <IoIosClose className="text-3xl pointer-events-none" />
+        </button>
+      </div>
     </div>
   );
 };
--- src/screens/chat/components/contacts-container/index.js
+++ src/screens/chat/components/contacts-container/index.js
@@ -8,82 +8,75 @@ import ContactList from "./components/ContactList";
 import CreateChannel from "./components/CreateChannel";
 
 const ContactsContainer = () => {
+  const directMessagesContacts = useAppStore(
+    (state) => state.directMessagesContacts
+  );
+  const updateFuncChat = useAppStore((state) => state.updateFuncChat);
+  const channels = useAppStore((state) => state.channels);
 
-  const directMessagesContacts = useAppStore(state => state.directMessagesContacts)
-  const updateFuncChat = useAppStore(state => state.updateFuncChat)
-  const channels = useAppStore(state => state.channels)
-
-  useEffect(()=>{
-
-    const getContacts = async()=>{
-
+  useEffect(() => {
+    const getContacts = async () => {
       const res = await apiClient.get(GET_CONTACTS_FOR_DM, {
-        withCredentials:true
-      })
+        withCredentials: true,
+      });
 
-      if(res.status === 200){
-        console.log("This are contacts:", res.data.contacts)
-        updateFuncChat({directMessagesContacts: [...res.data.contacts]})
+      if (res.status === 200) {
+        console.log("This are contacts:", res.data.contacts);
+        updateFuncChat({ directMessagesContacts: [...res.data.contacts] });
       }
-
-    }
-    const getUserChannels = async()=>{
-
+    };
+    const getUserChannels = async () => {
       const res = await apiClient.get(GET_USER_CHANNELS, {
-        withCredentials:true
-      })
+        withCredentials: true,
+      });
 
-      if(res.status === 201){
-        console.log("This are channels:", res.data.channels)
-        updateFuncChat({channels: res.data.channels})
+      if (res.status === 201) {
+        console.log("This are channels:", res.data.channels);
+        updateFuncChat({ channels: res.data.channels });
       }
+    };
 
-    }
-
-    getContacts()
-    getUserChannels()
-
-  },[updateFuncChat])
+    getContacts();
+    getUserChannels();
+  }, [updateFuncChat]);
 
   console.log("INside contacts container");
-  return <div className="relative md:w-[35vw] lg:w-[30vw] xl:w-[20vw] bg-[#1b1c24] border-r-2 border-[#2f303b] w-full">
-    <div className="p-3">
-      Logo
-    </div>
-    
-   <div className="my-5">
-    <div className="flex items-center justify-between pr-10">
-      <Title text={"Direct messages"} />
-      <NewDm />
-    </div>
+  return (
+    <div className="relative md:w-[35vw] lg:w-[30vw] xl:w-[20vw] bg-[#1b1c24] border-r-2 border-[#2f303b] w-full">
+      <div className="p-3">Logo</div>
 
-    <div className="max-h-[38vh] overflow-y-auto scrollbar-hidden">
-    <ContactList contacts={directMessagesContacts}/>
-    </div>
+      <div className="my-5">
+        <div className="flex items-center justify-between pr-10">
+          <Title text={"Direct messages"} />
+          <NewDm />
+        </div>
 
-   </div>
-   <div className="my-5">
-    <div className="flex items-center justify-between pr-10">
-      <Title text={"Channels"} />
-      <CreateChannel />
-    </div>
+        <div className="max-h-[38vh] overflow-y-auto scrollbar-hidden">
+          <ContactList contacts={directMessagesContacts} />
+        </div>
+      </div>
+      <div className="my-5">
+        <div className="flex items-center justify-between pr-10">
+          <Title text={"Channels"} />
+          <CreateChannel />
+        </div>
 
-    <div className="max-h-[38vh] overflow-y-auto scrollbar-hidden">
-    <ContactList contacts={channels} isChannel={true}/>
-    </div>
-   </div>
+        <div className="max-h-[38vh] overflow-y-auto scrollbar-hidden">
+          <ContactList contacts={channels} isChannel={true} />
+        </div>
+      </div>
 
-   <ProfileInfo />
-  </div>;
+      <ProfileInfo />
+    </div>
+  );
 };
 
- 
 export default ContactsContainer;
 
-
-
-
-const Title = ({text}) =>{
-  return <h6 className="uppercase tracking-widest text-neutral-400 pl-10 font-light text-opacity-90 text-sm">{text}</h6>
-}
-
+const Title = ({ text }) => {
+  return (
+    <h6 className="uppercase tracking-widest text-neutral-400 pl-10 font-light text-opacity-90 text-sm">
+      {text}
+    </h6>
+  );
+};
